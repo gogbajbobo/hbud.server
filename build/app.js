50625b6e3827a3456fc4c317f0f639057caad234
gogbajbobo <grigoriev.maxim@gmail.com>
2018-05-26 15:50:13 +0300

    build

--- build/app.js
+++ build/app.js
@@ -7,9 +7,11 @@ const config_1 = __importDefault(require("./internal/config"));
 const express = require("express");
 const routes_1 = __importDefault(require("./routes/routes"));
 const app = express();
+const logger_1 = __importDefault(require("./internal/logger"));
+const log = logger_1.default(module);
 app.use(routes_1.default);
 const port = process.env.PORT || config_1.default.get(`network:${process.env.appname}:port`) || 80;
 const server = app.listen(port, () => {
     const { address, port, family } = server.address();
-    console.log(`AUTH server listening at http://${address}:${port} ${family}`);
+    log.info(`AUTH server listening at http://${address}:${port} ${family}`);
 });
---
+++ build/internal/logger.js
@@ -0,0 +1,35 @@
+"use strict";
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const winston = __importStar(require("winston"));
+require('winston-daily-rotate-file');
+function logger(module) {
+    const path = module.filename.split('/').slice(-2).join('/'); //using filename in log statements
+    const consoleLogger = new winston.transports.Console({
+        colorize: true,
+        level: 'silly',
+        label: path
+    });
+    const dailyRotateFileLogger = new winston.transports.DailyRotateFile({
+        timestamp: true,
+        dirname: `../../hbud.logs/${process.env.appname}/`,
+        filename: `${process.env.appname}-%DATE%.log`,
+        datePattern: 'YYYY-MM-DD',
+        zippedArchive: false,
+        maxSize: '20m',
+        maxFiles: '3d',
+        level: 'info',
+        label: path,
+        json: false
+    });
+    let transports = [dailyRotateFileLogger];
+    process.env.NODE_ENV === 'production' || transports.push(consoleLogger);
+    return new winston.Logger({ transports });
+}
+exports.default = logger;
