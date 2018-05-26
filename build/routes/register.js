74d92e64efaabbe3378fd899a2e58aa8a077a2b6
gogbajbobo <grigoriev.maxim@gmail.com>
2018-05-26 15:27:55 +0300

    build

--- build/routes/register.js
+++ build/routes/register.js
@@ -2,6 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const registerRoute = (router) => {
     router.route('/register')
-        .get((req, res) => res.json({ info: 'hbud.api register' }));
+        .post((req, res) => res.json({ info: 'hbud.api register' }));
 };
 exports.default = registerRoute;
