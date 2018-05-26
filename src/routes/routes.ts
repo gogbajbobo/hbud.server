fdeb64f90bfd0924d2600ae4e6a319bdc41b8b5a
gogbajbobo <grigoriev.maxim@gmail.com>
2018-05-26 15:10:55 +0300

    simplify response

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -3,16 +3,6 @@ import { Router } from 'express';
 const router = Router();
 
 router.route('/')
-    .get((req, res) => {
-
-        const jsonData: Object = {
-            error: false,
-            message: 'OK!',
-            uri: process.env.SCRIPT_URI || 'localhost',
-            env: process.env
-        };
-        res.status(200).json(jsonData);
-
-    });
+    .get((req, res) => res.status(200).json({ api: 'hbud.api' }));
 
 export default router;
