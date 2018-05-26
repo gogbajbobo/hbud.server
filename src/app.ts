8f80dc279509749687c1432daf3b3790362bf6c7
gogbajbobo <grigoriev.maxim@gmail.com>
2018-05-26 15:11:33 +0300

    change console.log

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,16 @@
 import config from './internal/config'
 import express = require('express')
 import router from './routes/routes'
+import {AddressInfo} from "net";
 
 const app = express();
 app.use(router);
 
 const port = process.env.PORT || config.get(`network:${ process.env.appname }:port`) || 80;
 
-app.listen(port, () => {
-    console.log(`AUTH server start on port:${ port }`);
+const server = app.listen(port, () => {
+
+    const { address, port, family } = server.address() as AddressInfo;
+    console.log(`AUTH server listening at http://${ address }:${ port } ${ family }`)
+
 });
