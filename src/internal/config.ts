a435df74c92f421f03de27167986c6ddeb55517e
gogbajbobo <grigoriev.maxim@gmail.com>
2018-05-26 14:50:59 +0300

    src/internal/config.ts

---
+++ src/internal/config.ts
@@ -0,0 +1,10 @@
+import config from 'nconf'
+
+config
+    .file('../../hbud.config.json');
+
+config.get('env') && (process.env.NODE_ENV = config.get('env'));
+
+process.env.appname = 'auth';
+
+export default config;
