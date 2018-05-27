d66a18812be58bc0a62ecc02f0485f19868e87f8
gogbajbobo <grigoriev.maxim@gmail.com>
2018-05-27 10:37:04 +0300

    build

--- build/routes/register.js
+++ build/routes/register.js
@@ -2,6 +2,24 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const registerRoute = (router) => {
     router.route('/register')
-        .post((req, res) => res.json({ info: 'hbud.api register' }));
+        .get((req, res) => res.json({ info: 'hbud.api register get' }))
+        .post((req, res) => res.json({ info: 'hbud.api register post' }));
 };
 exports.default = registerRoute;
+// .post(passport.authenticate('jwt'), requireRole('admin'), (req, res) => {
+//
+//     const { username, password } = req.body;
+//     const role = req.body.role || 'user';
+//
+//     bcrypt.hash(password, 10, (err, hash) => {
+//
+//         users
+//             .insert({
+//                 username,
+//                 hash,
+//                 role
+//             })
+//             .then(result => res.status(200).json(result))
+//             .catch(err => catchErr(err, res))
+//
+//     })
