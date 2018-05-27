112ded8c5303c96f6550ea25b7bd7134fbade1b0
gogbajbobo <grigoriev.maxim@gmail.com>
2018-05-27 10:37:00 +0300

    internal/db.ts

---
+++ src/internal/db.ts
@@ -0,0 +1,17 @@
+import config from './config'
+import knex from 'knex'
+
+import logger from './logger'
+const log = logger(module);
+
+const mysqlConfig = config.get('mysql');
+
+log.debug(mysqlConfig);
+
+const db = knex({
+    client: 'mysql',
+    connection: mysqlConfig,
+    debug: false
+});
+
+export default knex
--- src/routes/register.ts
+++ src/routes/register.ts
@@ -1,11 +1,30 @@
 import { Router } from 'express'
+import db from '../internal/db'
 
 const registerRoute = (router: Router) => {
 
     router.route('/register')
-        .post((req, res) => res.json({ info: 'hbud.api register' }))
-
+        .get((req, res) => res.json({ info: 'hbud.api register get' }))
+        .post((req, res) => res.json({ info: 'hbud.api register post' }))
 
 };
 
 export default registerRoute
+
+    // .post(passport.authenticate('jwt'), requireRole('admin'), (req, res) => {
+    //
+    //     const { username, password } = req.body;
+    //     const role = req.body.role || 'user';
+    //
+    //     bcrypt.hash(password, 10, (err, hash) => {
+    //
+    //         users
+    //             .insert({
+    //                 username,
+    //                 hash,
+    //                 role
+    //             })
+    //             .then(result => res.status(200).json(result))
+    //             .catch(err => catchErr(err, res))
+    //
+    //     })
