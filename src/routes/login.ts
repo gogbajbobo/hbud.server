2ba8a80a406a99f3dc19aaeae3d1954185015e59
gogbajbobo <grigoriev.maxim@gmail.com>
2018-05-28 12:32:19 +0300

    invokeToken at login

--- src/routes/login.ts
+++ src/routes/login.ts
@@ -1,13 +1,19 @@
 import { Router } from 'express'
 import passport from '../internal/passport'
+import { UserModel } from "../internal/db";
+import token from '../internal/token'
 
 const loginRoute = (router: Router) => {
 
     router.route('/login')
-        .post(passport.authenticate('local'), (req, res) => res.json({ error: false, message: 'login success'}));
+        .post(passport.authenticate('local'), (req, res) => {
+            res.json(token.invokeToken(req.user as UserModel, 'Login success'))
+        });
 
     router.route('/logout')
-        .post(passport.authenticate('jwt'), (req, res) => res.json({ error: false, message: 'logout success' }));
+        .post(passport.authenticate('jwt'), (req, res) => {
+            res.json({ error: false, message: 'logout success' })
+        });
 
 };
 
