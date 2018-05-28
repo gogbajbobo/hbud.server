d6923c889181e9622d593b04eaa78e564806f119
gogbajbobo <grigoriev.maxim@gmail.com>
2018-05-28 12:31:50 +0300

    internal/token.ts

---
+++ src/internal/token.ts
@@ -0,0 +1,29 @@
+import jwt from 'jsonwebtoken'
+import config from './config'
+import { UserModel } from "./db";
+
+function invokeToken(user: UserModel, message: string): Object {
+
+    const tokenLifetime = 48 * 60 * 60; // seconds
+    const expirationTime = Math.floor(Date.now() / 1000) + tokenLifetime;
+
+    const userData = {
+        username: user.username,
+        role: user.role,
+        id: user.id,
+        exp: expirationTime
+    };
+
+    const accessToken = jwt.sign(userData, config.get('jwt:secretKey'));
+
+    return {
+        error: false,
+        message: message || 'Token invoked successfully',
+        user: userData,
+        accessToken,
+        expirationTime
+    };
+
+}
+
+export default { invokeToken };
