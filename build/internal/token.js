b92d4fcd8e37f400ec15a730a93c9f880d449d48
gogbajbobo <grigoriev.maxim@gmail.com>
2018-05-28 12:32:30 +0300

    build

---
+++ build/internal/token.js
@@ -0,0 +1,26 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+const config_1 = __importDefault(require("./config"));
+function invokeToken(user, message) {
+    const tokenLifetime = 48 * 60 * 60; // seconds
+    const expirationTime = Math.floor(Date.now() / 1000) + tokenLifetime;
+    const userData = {
+        username: user.username,
+        role: user.role,
+        id: user.id,
+        exp: expirationTime
+    };
+    const accessToken = jsonwebtoken_1.default.sign(userData, config_1.default.get('jwt:secretKey'));
+    return {
+        error: false,
+        message: message || 'Token invoked successfully',
+        user: userData,
+        accessToken,
+        expirationTime
+    };
+}
+exports.default = { invokeToken };
--- build/routes/login.js
+++ build/routes/login.js
@@ -4,10 +4,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const passport_1 = __importDefault(require("../internal/passport"));
+const token_1 = __importDefault(require("../internal/token"));
 const loginRoute = (router) => {
     router.route('/login')
-        .post(passport_1.default.authenticate('local'), (req, res) => res.json({ error: false, message: 'login success' }));
+        .post(passport_1.default.authenticate('local'), (req, res) => {
+        res.json(token_1.default.invokeToken(req.user, 'Login success'));
+    });
     router.route('/logout')
-        .post(passport_1.default.authenticate('jwt'), (req, res) => res.json({ error: false, message: 'logout success' }));
+        .post(passport_1.default.authenticate('jwt'), (req, res) => {
+        res.json({ error: false, message: 'logout success' });
+    });
 };
 exports.default = loginRoute;
