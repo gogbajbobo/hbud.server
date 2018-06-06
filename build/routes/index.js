5fd92359f547aec00732fbfc2d95fd04c750a983
gogbajbobo <grigoriev.maxim@gmail.com>
2018-06-06 12:59:55 +0300

    build

--- build/routes/index.js
+++ build/routes/index.js
@@ -4,22 +4,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
+const cors_1 = __importDefault(require("./cors"));
 const root_1 = __importDefault(require("./root"));
 const register_1 = __importDefault(require("./register"));
 const login_1 = __importDefault(require("./login"));
 const router = express_1.Router();
-router.route('*')
-    .all((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*"); //TODO: * — is dangerous?
-    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-type, Authorization');
-    if (req.method === 'OPTIONS') {
-        res.status(200).end();
-    }
-    else {
-        next();
-    }
-});
+cors_1.default(router);
 root_1.default(router);
 register_1.default(router);
 login_1.default(router);
