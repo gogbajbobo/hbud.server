9b07c05373af3313f38ff99784c2a2531d7dbf51
gogbajbobo <grigoriev.maxim@gmail.com>
2018-06-06 18:03:32 +0300

    use functions in routes/auth/register.ts

--- src/routes/auth/register.ts
+++ src/routes/auth/register.ts
@@ -4,38 +4,24 @@ import bcrypt from 'bcryptjs'
 
 import db from '../../internal/db'
 import passport from '../../internal/passport'
-
-function requireRole(role: string) {
-
-    return (req: Request, res: Response, next: NextFunction) => {
-
-        console.log(req.user);
-
-        req.user
-            ? role.includes(req.user.role)
-                ? next()
-                : res.status(401).send('Unauthorized').end()
-            : res.status(403).send('Forbidden').end()
-
-    }
-
-}
+import fn from '../functions'
 
 const registerRoute = (router: Router, rootPath: string) => {
 
     router.route(`${ rootPath }/register`)
 
-        .all(passport.authenticate('jwt'), requireRole('admin'))
+        .all(passport.authenticate('jwt'), fn.requireRole('admin'))
 
         .get((req, res) => res.render('register'))
 
         .post((req, res) => {
 
             const {username, password} = req.body;
-            const role = req.body.role || 'visitor';
 
             if (!username || !password)
                 return res.status(400).json({error: true, code: 400, message: 'Bad Request'});
+
+            const role = req.body.role || 'visitor';
 
             bcrypt.hash(password, 10, (err, hash) => {
 
@@ -46,7 +32,7 @@ const registerRoute = (router: Router, rootPath: string) => {
                         role
                     })
                     .then(result => res.json(result))
-                    .catch(err => res.status(500).json({error: true, message: err.toLocaleString()}))
+                    .catch(err => fn.catchErr(err, res))
 
             })
 
