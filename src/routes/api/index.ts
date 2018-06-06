f3b956afa09c4d2443343b7ec583661c74faf2d7
gogbajbobo <grigoriev.maxim@gmail.com>
2018-06-06 18:04:12 +0300

    usersRoutes

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -1,11 +1,16 @@
 import { Router } from 'express'
 
+import usersRoutes from './users'
+import passport from "../../internal/passport";
+
 const apiPath = '/api';
 
 const apiRoutes = (router: Router) => {
 
     router.route(`${ apiPath }/*`)
-        .all((req, res, next) => next())
+        .all(passport.authenticate('jwt'), (req, res, next) => next());
+
+    usersRoutes(router, apiPath);
 
 };
 
