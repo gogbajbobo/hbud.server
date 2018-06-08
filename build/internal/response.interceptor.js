1336fc35061b450ce5ad13155e5c27f6e85a7936
gogbajbobo <grigoriev.maxim@gmail.com>
2018-06-08 11:18:49 +0300

    build

--- build/internal/response.interceptor.js
+++ build/internal/response.interceptor.js
@@ -16,8 +16,8 @@ function responseFinished() {
     };
 }
 function logRequest(req, res) {
-    log.transports.console.level = 'error';
+    process.env.NODE_ENV === 'production' || (log.transports.console.level = 'error');
     log.info(req.method, req.originalUrl, res.statusCode, res.getHeader('Content-Length'), req.get('User-Agent'), req.ip);
-    log.transports.console.level = 'silly';
+    process.env.NODE_ENV === 'production' || (log.transports.console.level = 'silly');
 }
 exports.default = responseFinished;
