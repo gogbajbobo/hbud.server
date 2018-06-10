0f2a1326dd625a4c589f09de20bc7a225ceceb24
gogbajbobo <grigoriev.maxim@gmail.com>
2018-06-10 19:31:43 +0300

    build

--- build/routes/auth/register.js
+++ build/routes/auth/register.js
@@ -10,21 +10,34 @@ const functions_1 = __importDefault(require("../functions"));
 const registerRoute = (router, rootPath) => {
     router.route(`${rootPath}/register`)
         .all(passport_1.default.authenticate('jwt'), functions_1.default.requireRole('admin'))
-        .get((req, res) => res.render('register'))
+        .get((req, res) => {
+        db_1.default('roles')
+            .select()
+            .then(roles => res.render('register', { roles }))
+            .catch(err => functions_1.default.catchErr(err, res));
+    })
         .post((req, res) => {
-        const { username, password } = req.body;
-        if (!username || !password)
+        const { username, password, roles } = req.body;
+        if (!username || !password || !roles)
             return res.status(400).json({ error: true, code: 400, message: 'Bad Request' });
-        const role = req.body.role || 'visitor';
         bcryptjs_1.default.hash(password, 10, (err, hash) => {
-            db_1.default('users')
-                .insert({
-                username,
-                hash,
-                role
+            db_1.default.transaction(trx => {
+                return trx
+                    .insert({ username, hash })
+                    .into('users')
+                    .then(result => {
+                    const userId = result[0], users_roles = roles.map((roleId) => {
+                        return { users_id: userId, roles_id: roleId };
+                    });
+                    return trx
+                        .insert(users_roles)
+                        .into('users_roles')
+                        .then(result => console.log(result))
+                        .catch(err => functions_1.default.catchErr(err, res));
+                });
             })
-                .then(result => res.json(result))
-                .catch(err => functions_1.default.catchErr(err, res));
+                .then(result => res.json({ result }))
+                .catch(err => console.error(err.message));
         });
     });
 };
