b81ef07a670f3a3307d6c85c3689096f2053f9f6
gogbajbobo <grigoriev.maxim@gmail.com>
2018-06-18 14:28:05 +0300

    transports: ['websocket']

--- build/app.js
+++ build/app.js
@@ -34,7 +34,7 @@ log.info(`host: ${host} / port: ${port}`);
 //
 // });
 const ioServer = new http_1.default.Server(app);
-const io = socket_io_1.default(ioServer);
+const io = socket_io_1.default(ioServer, { transports: ['websocket'] });
 const ioPort = config_1.default.get(`network:${process.env.appname}:ioPort`);
 ioServer.listen(ioPort, host, () => {
     log.info(`socket.io listening on port ${ioPort}`);
