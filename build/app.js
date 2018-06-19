2048e303a70714256676f9043fd2b1099c7d0a4b
gogbajbobo <grigoriev.maxim@gmail.com>
2018-06-19 13:10:30 +0300

    build

--- build/app.js
+++ build/app.js
@@ -5,7 +5,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const config_1 = __importDefault(require("./internal/config"));
 const express_1 = __importDefault(require("express"));
-const http_1 = __importDefault(require("http"));
 const socket_io_1 = __importDefault(require("socket.io"));
 const routes_1 = __importDefault(require("./routes"));
 const app = express_1.default();
@@ -27,22 +26,25 @@ const host = config_1.default.get(`network:${process.env.appname}:hostname`);
 log.info(`appname: ${process.env.appname}`);
 log.info(`NODE_ENV: ${process.env.NODE_ENV}`);
 log.info(`host: ${host} / port: ${port}`);
-// const server = app.listen(port, host, () => {
-//
-//     const { address, port, family } = server.address() as AddressInfo;
-//     log.info(`HBUD server listening at http://${ address }:${ port } ${ family }`)
-//
-// });
-const ioServer = new http_1.default.Server(app);
-const io = socket_io_1.default(ioServer);
-const ioPort = config_1.default.get(`network:${process.env.appname}:ioPort`);
-ioServer.listen(ioPort, host, () => {
-    log.info(`socket.io listening on port ${ioPort}`);
+const server = app.listen(port, host, () => {
+    const { address, port, family } = server.address();
+    log.info(`HBUD server listening at http://${address}:${port} ${family}`);
 });
-io.on('connection', socket => {
-    log.info(`socket connected`);
-    socket.emit('test', { test: 'data' });
-    socket.on('test', data => {
-        log.debug(`test data: ${data}`);
+const io = socket_io_1.default(server);
+io.on('connect', socket => {
+    log.info(`socket connected ${socket.id}`);
+    const authTimer = setTimeout(() => socket.disconnect(true), 1000);
+    socket.on('authorize', (data, ack) => {
+        if (!data.token) {
+            const msg = `no token`;
+            log.info(msg);
+            ack(msg);
+            return;
+        }
+        log.debug(`authorize ${JSON.stringify(data)}`);
+        clearTimeout(authTimer);
+    });
+    socket.on('disconnect', () => {
+        log.info(`disconnect socket ${socket.id}`);
     });
 });
