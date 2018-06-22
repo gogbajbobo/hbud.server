aec836543fa261ed50028b454b73caed5887117b
gogbajbobo <grigoriev.maxim@gmail.com>
2018-06-22 15:13:35 +0300

    check auth token

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -1,62 +1,68 @@
 import sio, {Socket} from 'socket.io'
-const sio_auth = require('socketio-auth');
-
 import logger from '../internal/logger'
-import * as http from "http";
+import * as http from "http"
+import tokenService from '../internal/token'
+import Users from "../internal/db/users";
+
+const sio_auth = require('socketio-auth');
 const log = logger(module);
 
 function socketStart(server: http.Server) {
 
     const io = sio(server);
-
-    sio_auth(io, {
-        authenticate: function (socket: Socket, data: any, callback: Function) {
-
-            console.log(`recive ${ data }`);
-            return callback(new Error("User not found"))
-
-            //get credentials sent by the client
-            // var username = data.username;
-            // var password = data.password;
-
-            // db.findUser('User', {username:username}, function(err, user) {
-            //
-            //     if (err || !user) return callback(new Error("User not found"));
-            //     return callback(null, user.password == password);
-            //     //inform the callback of auth success/failure
-            // });
-        }
-    });
-
-    io.on('connect', socket => {
-
-        log.info(`socket connected ${ socket.id }`);
-
-        const authTimer = setTimeout(() => socket.disconnect(true), 1000);
-
-        socket.on('authorize', (data, ack) => {
-
-            if (!data.token) {
-
-                const msg = `no token`;
-                log.info(msg);
-                ack(msg);
-
-                return
-
-            }
-
-            log.debug(`authorize ${ JSON.stringify(data) }`);
-            clearTimeout(authTimer)
-
-        });
-
-        socket.on('disconnect', () => {
-            log.info(`disconnect socket ${ socket.id }`);
-        })
-
-    });
+    sio_auth(io, { authenticate });
+    io.on('connect', listener)
 
 }
+
+const listener = (socket: Socket): void => {
+
+    log.info(`socket connected ${ socket.id }`);
+
+    socket.on('authenticated', () => {
+        log.info(`socket authenticated ${ socket.id }`)
+    });
+
+    socket.on('disconnect', () => {
+        log.info(`disconnect socket ${ socket.id }`);
+    })
+
+};
+
+const authenticateLog = (err: Error|null, success: boolean, callback: Function) => {
+
+    if (!success) {
+        if (err) {
+            log.info(`Authentication error: ${ err.message }`);
+        } else {
+            log.info('Authentication failure');
+        }
+    }
+
+    return callback(err, success)
+
+};
+
+const authenticate = (socket: Socket, data: any, callback: Function) => {
+
+    const { token } = data;
+    const jwtPayload = tokenService.extractData(token);
+
+    tokenService.checkJwtPayload(jwtPayload, (err: Error) => {
+
+        if (err) return authenticateLog(err, false, callback);
+
+        Users.getUsersWithRoles(['*'], { username: jwtPayload.username })
+            .then(users => {
+
+                const user = users[0];
+                Promise.resolve(authenticateLog(null, user ? !user.reauth : false, callback))
+
+            })
+            .catch(err => Promise.resolve(authenticateLog(err, false, callback)))
+
+    });
+
+};
 
 export default { socketStart }
