628b71dfcb337e0a4930f083946ec5e5869f5e0b
gogbajbobo <grigoriev.maxim@gmail.com>
2018-06-23 19:50:02 +0300

    build

--- build/internal/cache.js
+++ build/internal/cache.js
@@ -4,13 +4,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const node_cache_1 = __importDefault(require("node-cache"));
+const difference_in_seconds_1 = __importDefault(require("date-fns/difference_in_seconds"));
 class SocketUserCache extends node_cache_1.default {
     constructor(options) {
         super(options);
     }
     set(key, value) {
-        console.log(`set ${JSON.stringify(arguments)}`);
-        return super.set(key, value);
+        const tokenExpDate = new Date(value.tokenPayload.exp * 1000);
+        const ttl = difference_in_seconds_1.default(tokenExpDate, Date.now());
+        return super.set(key, value, ttl);
     }
     get(key) {
         return super.get(key);
--- build/socket/index.js
+++ build/socket/index.js
@@ -8,6 +8,7 @@ const logger_1 = __importDefault(require("../internal/logger"));
 const token_1 = __importDefault(require("../internal/token"));
 const users_1 = __importDefault(require("../internal/db/users"));
 const cache_1 = __importDefault(require("../internal/cache"));
+const is_past_1 = __importDefault(require("date-fns/is_past"));
 const sio_auth = require('socketio-auth');
 const log = logger_1.default(module);
 function socketStart(server) {
@@ -15,7 +16,7 @@ function socketStart(server) {
     sio_auth(io, { authenticate });
     io.on('connect', listener);
 }
-const authenticate = (socket, data, callback) => {
+function authenticate(socket, data, callback) {
     const { token } = data;
     const tokenPayload = token_1.default.extractData(token);
     token_1.default.checkJwtPayload(tokenPayload, (err) => {
@@ -23,29 +24,50 @@ const authenticate = (socket, data, callback) => {
             return authenticateLog(err, false, callback);
         users_1.default.getUsersWithRoles(['*'], { username: tokenPayload.username })
             .then(users => {
-            const user = users[0];
-            const userIsGood = user ? !user.reauth : false;
+            const user = users[0], userIsGood = user ? !user.reauth : false;
             userIsGood && cache_1.default.set(socket.id, { user, tokenPayload });
             Promise.resolve(authenticateLog(null, userIsGood, callback));
         })
             .catch(err => Promise.resolve(authenticateLog(err, false, callback)));
     });
-};
-const authenticateLog = (err, success, callback) => {
+}
+function authenticateLog(err, success, callback) {
     if (!success) {
         log.error(err ? `Authentication error: ${err.message}` : 'Authentication failure');
     }
     return callback(err, success);
-};
-const listener = (socket) => {
+}
+function listener(socket) {
     log.info(`socket connected ${socket.id}`);
-    socket.on('authenticated', () => {
-        const user = cache_1.default.get(socket.id).user;
-        log.info(`socket authenticated ${socket.id} | ${user.username}`);
-    });
+    socket.on('authenticated', () => socketAuthenticated(socket));
     socket.on('disconnect', () => {
         cache_1.default.del(socket.id);
         log.info(`disconnect socket ${socket.id}`);
     });
-};
+}
+function socketAuthenticated(socket) {
+    const socketUserData = cache_1.default.get(socket.id), user = socketUserData.user;
+    log.info(`socket authenticated ${socket.id} | ${user.username}`);
+    socketUseTokenTTLCheck(socket);
+    socketRoutes(socket);
+}
+function socketUseTokenTTLCheck(socket) {
+    socket.use((packet, next) => {
+        if (isTokenExpire(socket.id)) {
+            socket.emit('unauthorized', { message: 'token expire' });
+            return socket.disconnect(true);
+        }
+        next();
+    });
+}
+function isTokenExpire(socketId) {
+    const socketUserData = cache_1.default.get(socketId);
+    return is_past_1.default(new Date(socketUserData.tokenPayload.exp * 1000));
+}
+function socketRoutes(socket) {
+    socket.on('test', (ack) => {
+        console.log('test event');
+        ack();
+    });
+}
 exports.default = { socketStart };
